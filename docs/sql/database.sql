a236571984afc1ea22a4265f28a1e393d10ab34e
LiusCraft <liuscraft@qq.com>
2024-01-09 22:34:36 +0800

    docs: add article table create sql

--- docs/sql/database.sql
+++ docs/sql/database.sql
@@ -1,4 +1,5 @@
 drop table if exists user;
+
 create table user (
     id int auto_increment comment 'primarykey',
     name varchar(20) null,
@@ -12,6 +13,7 @@ create table user (
 ) comment 'community user';
 
 drop table if exists invite_code;
+
 create table invite_code (
     id int auto_increment comment 'primarykey',
     code varchar(20) null,
@@ -21,3 +23,17 @@ create table invite_code (
     constraint invite_code_pk primary key (id)
 ) comment 'Cache invite code for register';
 
+drop table if exists article;
+create table article (
+    id int auto_increment comment 'primarykey',
+    title varchar(50) null,
+    `description` varchar(5000) null,
+    user_id int not null,
+    issue_id int default 0,
+    solved tinyint(1) default 0,
+    `like` int default 0,
+    created_at datetime null,
+    updated_at datetime null,
+    deleted_at datetime null,
+    constraint article_pk primary key (id)
+) comment 'it is issue or answer';

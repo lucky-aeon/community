28081b0da2b5366d2b54f1852ddbfd4125b3820c
LiusCraft <liuscraft@qq.com>
2024-01-27 17:50:18 +0800

    feat(article): add article list

--- web/src/views/article/public/index.vue
+++ web/src/views/article/public/index.vue
@@ -1,12 +1,158 @@
 <template>
-    <div class="container">
-      
-    </div>
-  </template>
-  
+  <div :style="{ background: 'var(--color-fill-2)', padding: '28px' }" >
+    <a-page-header
+      :style="{ background: 'var(--color-bg-2)' }"
+      title="ArcoDesign"
+      subtitle="ArcoDesign Vue 2.0"
+      :show-back="false"
+    >
+      <template #breadcrumb>
+        <a-breadcrumb>
+          <a-breadcrumb-item>Home</a-breadcrumb-item>
+          <a-breadcrumb-item>Channel</a-breadcrumb-item>
+          <a-breadcrumb-item>News</a-breadcrumb-item>
+        </a-breadcrumb>
+      </template>
+      <template #extra>
+        <a-cascader :options="options" :style="{width:'320px'}" placeholder="Please select ..." allow-search/>
+      </template>
+    </a-page-header>
+    <a-card>
+      <a-list class="list-demo-action-layout" :bordered="false" :data="dataSource" :pagination-props="paginationProps">
+      <template #item="{ item }">
+        <a-list-item class="list-demo-item" action-layout="vertical">
+          <template #actions>
+            <span><icon-heart />83</span>
+            <span><icon-star />{{ item.index }}</span>
+            <span><icon-message />Reply</span>
+          </template>
+          <template #extra>
+            <div className="image-area">
+              <img alt="arco-design" :src="item.imageSrc" />
+            </div>
+          </template>
+          <a-list-item-meta :title="item.title" :description="item.description">
+            <template #avatar>
+              <a-avatar shape="square">
+                <img alt="avatar" :src="item.avatar" />
+              </a-avatar>
+            </template>
+          </a-list-item-meta>
+        </a-list-item>
+      </template>
+    </a-list>
+    </a-card>
+  </div>
+    
+</template>
 
-  <script lang="ts">
-    export default {
-      name: 'public', // If you want the include property of keep-alive to take effect, you must name the component
-    };
-  </script>
+<script lang="ts">
+import { reactive } from 'vue';
+
+const names = ['Socrates', 'Balzac', 'Plato'];
+const avatarSrc = [
+  '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/a8c8cdb109cb051163646151a4a5083b.png~tplv-uwbnlip3yd-webp.webp',
+  '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/e278888093bef8910e829486fb45dd69.png~tplv-uwbnlip3yd-webp.webp',
+  '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/9eeb1800d9b78349b24682c3518ac4a3.png~tplv-uwbnlip3yd-webp.webp',
+];
+const imageSrc = [
+  '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/29c1f9d7d17c503c5d7bf4e538cb7c4f.png~tplv-uwbnlip3yd-webp.webp',
+  '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/04d7bc31dd67dcdf380bc3f6aa07599f.png~tplv-uwbnlip3yd-webp.webp',
+  '//p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/1f61854a849a076318ed527c8fca1bbf.png~tplv-uwbnlip3yd-webp.webp',
+];
+const dataSource = new Array(15).fill(null).map((_, index) => {
+  return {
+    index,
+    avatar: avatarSrc[index % avatarSrc.length],
+    title: names[index % names.length],
+    description:
+      'Beijing ByteDance Technology Co., Ltd. is an enterprise located in China. ByteDance has products such as TikTok, Toutiao, volcano video and Douyin (the Chinese version of TikTok).',
+    imageSrc: imageSrc[index % imageSrc.length],
+  };
+});
+
+export default {
+  setup() {
+     const options = [
+      {
+        value: 'beijing',
+        label: 'Beijing',
+        children: [
+          {
+            value: 'chaoyang',
+            label: 'ChaoYang',
+            children: [
+              {
+                value: 'datunli',
+                label: 'Datunli',
+              },
+            ],
+          },
+          {
+            value: 'haidian',
+            label: 'Haidian',
+          },
+          {
+            value: 'dongcheng',
+            label: 'Dongcheng',
+          },
+          {
+            value: 'xicheng',
+            label: 'Xicheng',
+            children: [
+              {
+                value: 'jinrongjie',
+                label: 'Jinrongjie',
+              },
+              {
+                value: 'tianqiao',
+                label: 'Tianqiao',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        value: 'shanghai',
+        label: 'Shanghai',
+        children: [
+          {
+            value: 'huangpu',
+            label: 'Huangpu',
+          },
+        ],
+      },
+    ];
+    return {
+      options,
+      dataSource,
+      paginationProps: reactive({
+        defaultPageSize: 3,
+        total: dataSource.length
+      })
+    }
+  },
+}
+</script>
+
+<style scoped>
+.list-demo-action-layout .image-area {
+  width: 183px;
+  height: 119px;
+  border-radius: 2px;
+  overflow: hidden;
+}
+
+.list-demo-action-layout .list-demo-item {
+  padding: 20px 0;
+  border-bottom: 1px solid var(--color-fill-3);
+}
+
+.list-demo-action-layout .image-area img {
+  width: 100%;
+}
+
+.list-demo-action-layout .arco-list-item-action .arco-icon {
+  margin: 0 4px;
+}
+</style>

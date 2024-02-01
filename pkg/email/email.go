62094eb45df45b4d198fb87cb37552b40cf7b202
Xhy <1416318023@qq.com>
2024-02-01 23:59:01 +0800

    feat: add email module

---
+++ pkg/email/email.go
@@ -0,0 +1,40 @@
+package email
+
+import (
+	"github.com/jordan-wright/email"
+	"log"
+	"net/smtp"
+	"time"
+)
+
+var emailPoll *email.Pool
+
+var from string
+
+func Init(address, username, password, host string, pollCount int) {
+	p, err := email.NewPool(
+		address,
+		pollCount,
+		smtp.PlainAuth("", username, password, host),
+	)
+	if err != nil {
+		log.Fatalln("email connect fail", err.Error())
+		return
+	}
+	emailPoll = p
+	from = username
+}
+
+func Send(to []string, content, subject string) {
+	go func(to []string, content, subject string) {
+		e := email.NewEmail()
+		e.From = from
+		e.To = to
+		e.Subject = subject
+		e.Text = []byte(content)
+		err := emailPoll.Send(e, 10*time.Second)
+		if err != nil {
+			return
+		}
+	}(to, content, subject)
+}

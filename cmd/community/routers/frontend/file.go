a463fb1da4df2498c4c562dfdb5715d9f67594ff
Xhy <1416318023@qq.com>
2024-03-29 21:34:31 +0800

    fix(文件模块): 调整 uuid ttl

--- cmd/community/routers/frontend/file.go
+++ cmd/community/routers/frontend/file.go
@@ -96,7 +96,7 @@ func getPolicy(ctx *gin.Context) {
 	signedStr := base64.StdEncoding.EncodeToString(h.Sum(nil))
 
 	uId := uuid.NewString()
-	cache.GetInstance().Set(uId, 1, 10*time.Second)
+	cache.GetInstance().Set(uId, 1, 5*time.Second)
 	body := fmt.Sprintf("{\"fileKey\":${object},\"size\":${size},\"mimeType\":${mimeType},\"x:userId\":%d,\"x:uuid\":\"%s\"}", userId, uId)
 
 	var callbackParam CallbackParam

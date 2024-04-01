7f9f0f78b30c655e72b55a8ac94958ab9f9ccd1f
LiusCraft <meliuscraft@gmail.com>
2024-04-01 23:37:05 +0800

    feat(article): add catalog card and show @

--- web/src/views/consumer/qa/QaView.vue
+++ web/src/views/consumer/qa/QaView.vue
@@ -59,8 +59,7 @@
           </a-space>
         </div>
       </a-page-header>
-      <div id="markdown-container"></div>
-      <a-card :bordered="false" style="margin-top: 10px;">
+      <MarkdownEdit :render-nav="setArticleNavs" preview-only :show-nav="false" v-model="articleData.content" />      <a-card :bordered="false" style="margin-top: 10px;">
         <comment-edit :callback="getRootComment" :article-id="articleData.id"  style="margin-bottom: 15px;"/>
         <template v-if="articleCommentList.length">
           <comment-item :article="articleData" :callback="getRootComment" v-for="comment in articleCommentList"
@@ -75,11 +74,19 @@
     </a-col>
     <a-col span="7">
       <UserInfoCard :user-id="articleData.user.id" />
+      <a-affix>
+      <a-card style="margin-top: 10px;max-height: 400px;" :bordered="false" title="目录">
+        <a-anchor smooth style="width: 100%;max-height: 300px;" >
+          <MyAnchorLink v-for="item in articleNavs" :data="item" style="width: 100%;"/>
+      </a-anchor>
+      </a-card>
+    </a-affix>
     </a-col>
   </a-row>
 </template>
 
 <script setup>
+import MyAnchorLink from '@/components/MyAnchorLink.vue'
 import { apiSubscribe, apiSubscribeState, } from "@/apis/apiSubscribe.js";
 import { apiArticleLike, apiArticleLikeState, apiArticleView } from '@/apis/article';
 import { apiGetArticleComment } from '@/apis/comment';
@@ -91,107 +98,11 @@ import router from "@/router";
 import { IconDown, IconThumbUp, IconThumbUpFill } from '@arco-design/web-vue/es/icon';
 import Cherry from 'cherry-markdown';
 import 'cherry-markdown/dist/cherry-markdown.css';
-import { nextTick, onMounted, ref, watch } from 'vue';
+import { nextTick, ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
-var CustomHookA = Cherry.createSyntaxHook('codeBlock', Cherry.constants.HOOKS_TYPE_LIST.PAR, {
-  makeHtml(str) {
-    console.warn('custom hook', 'hello');
-    return str;
-  },
-  rule() {
-    const regex = {
-      begin: '',
-      content: '',
-      end: '',
-    };
-    regex.reg = new RegExp(regex.begin + regex.content + regex.end, 'g');
-    return regex;
-  },
-});
+import MarkdownEdit from '@/components/MarkdownEdit.vue';
 const getFileUrl = (fileKey) => apiGetFile(fileKey)
-
-var cherryConfig = {
-  id: 'markdown-container',
-  value: '# welcome to cherry editor! \n awdwadad',
-  externals: {
-    echarts: window.echarts,
-    katex: window.katex,
-    MathJax: window.MathJax,
-  },
-  engine: {
-    global: {
-      urlProcessor(url, srcType) {
-        console.log(`url-processor`, url, srcType);
-        return url;
-      },
-    },
-    syntax: {
-      fontEmphasis: {
-        allowWhitespace: true, // 是否允许首尾空格
-      },
-      mathBlock: {
-        engine: 'MathJax', // katex或MathJax
-        src: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js', // 如果使用MathJax将js在此处引入，katex则需要将js提前引入
-      },
-      inlineMath: {
-        engine: 'MathJax', // katex或MathJax
-      },
-      emoji: {
-        useUnicode: false,
-        customResourceURL: 'https://github.githubassets.com/images/icons/emoji/unicode/${code}.png?v8',
-        upperCase: true,
-      },
-      // toc: {
-      //     tocStyle: 'nested'
-      // }
-      // 'header': {
-      //   strict: false
-      // }
-    },
-    customSyntax: {
-      // SyntaxHookClass
-      CustomHook: {
-        syntaxClass: CustomHookA,
-        force: false,
-        after: 'br',
-      },
-    },
-  },
-  toolbars: {
-    toolbar: false,
-    toc: {
-      updateLocationHash: true, // 要不要更新URL的hash
-      defaultModel: 'full', // pure: 精简模式/缩略模式，只有一排小点； full: 完整模式，会展示所有标题
-    },
-  },
-  editor: {
-    defaultModel: 'previewOnly',
-  },
-  callback: {
-    onClickPreview: function (e) {
-      const { target } = e;
-      if (target.tagName === 'IMG') {
-        console.log('click img', target);
-        // eslint-disable-next-line no-undef
-        const tmp = new Viewer(target, {
-          button: false,
-          navbar: false,
-          title: [1, (image, imageData) => `${image.alt.replace(/#.+$/, '')} (${imageData.naturalWidth} × ${imageData.naturalHeight})`],
-          hidden() {
-            tmp.destroy()
-          },
-        });
-        tmp.show();
-      }
-    }
-  },
-  previewer: {
-    // 自定义markdown预览区域class
-    // className: 'markdown'
-  },
-  keydown: [],
-  //extensions: [],
-};
+const articleNavs = ref([])
 
 const articleData = ref(null)
 const route = useRoute()
@@ -258,7 +169,29 @@ function subscribe(eventId, businessId) {
   })
 }
 
+function arrayToTree(arr, level = 1) {
+  const tree = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    if(level != 1 && item.level == 1) break
+    if (item.level === level) {
+      const node = {
+        id: item.id,
+        text: item.text,
+        children: arrayToTree(arr.slice(i + 1), level + 1)
+      };
+      tree.push(node);
+    }
+  }
+
+  return tree;
+}
+
+function setArticleNavs(arr) {
+  console.log(arr)
+  articleNavs.value =  arrayToTree(arr)
+}
 </script>
 
 <style>
--- web/src/views/layout/ConsumerLayout.vue
+++ web/src/views/layout/ConsumerLayout.vue
@@ -3,14 +3,16 @@
     <a-layout style="height: 100%;">
       <a-layout-header>
         <a-menu class="line" mode="horizontal" :default-selected-keys="[]">
-          <RouterLink to="/admin" v-if="userStore.userInfo.role == 'admin'"><a-menu-item key="1">后台管理</a-menu-item></RouterLink> 
+          <RouterLink to="/admin" v-if="userStore.userInfo.role == 'admin'"><a-menu-item key="1">后台管理</a-menu-item>
+          </RouterLink>
           <AButtonGroup style="float: right;">
 
 
             <a-tooltip content="消息通知">
 
-              <AButton type="text" @click="setPopoverVisible"><a-badge :count="messageStore.unReadCount" :offset="[5, -5]"><icon-notification size="large" class="nav-btn"
-                    type="outline" :shape="'circle'" /></a-badge></AButton>
+              <AButton type="text" @click="setPopoverVisible"><a-badge :count="messageStore.unReadCount"
+                  :offset="[5, -5]"><icon-notification size="large" class="nav-btn" type="outline"
+                    :shape="'circle'" /></a-badge></AButton>
 
             </a-tooltip>
 
@@ -78,34 +80,27 @@
             </a-sub-menu>
           </a-menu>
         </a-layout-sider>
-        <a-layout-content style="padding: 15px; background-color: #f2f3f5;">
+        <a-layout-content id="consumerlayout" style="padding: 15px; background-color: #f2f3f5;">
           <RouterView />
         </a-layout-content>
       </a-layout>
-      <a-layout-footer><a-card hoverable >
-    <div
-      :style="{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-      }"
-    >
-      <span
-        :style="{ display: 'flex', alignItems: 'center', color: '#1D2129' }"
-      >
-        <a-avatar
-          :style="{ marginRight: '8px', backgroundColor: '#165DFF' }"
-          :size="28"
-          image-url="/icp.png"
-        >
-          A
-        </a-avatar>
-        <a-typography-text>    <a-link href="https://beian.mps.gov.cn/#/query/webSearch?code=42088102000211" rel="noreferrer" target="_blank">鄂公网安备42088102000211</a-link>
-</a-typography-text>
-      </span>
-      <a-link>Power by Xhy, 2024-至今</a-link>
-    </div>
-  </a-card></a-layout-footer>
+      <a-layout-footer><a-card hoverable>
+          <div :style="{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }">
+            <span :style="{ display: 'flex', alignItems: 'center', color: '#1D2129' }">
+              <a-avatar :style="{ marginRight: '8px', backgroundColor: '#165DFF' }" :size="28" image-url="/icp.png">
+                A
+              </a-avatar>
+              <a-typography-text> <a-link href="https://beian.mps.gov.cn/#/query/webSearch?code=42088102000211"
+                  rel="noreferrer" target="_blank">鄂公网安备42088102000211</a-link>
+              </a-typography-text>
+            </span>
+            <a-link>Power by Xhy, 2024-至今</a-link>
+          </div>
+        </a-card></a-layout-footer>
     </a-layout>
   </div>
 </template>
@@ -136,7 +131,7 @@ userStore.getMenu()
 
 
 function clearMsg() {
-  apiClearUnReadMsg(msgType.value).then(({ok})=> {if(ok) msgReload.value++})
+  apiClearUnReadMsg(msgType.value).then(({ ok }) => { if (ok) msgReload.value++ })
 }
 
 

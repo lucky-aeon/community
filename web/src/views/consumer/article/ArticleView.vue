7256a806ec5287ec524e21fe4fed9b23cd308d6c
LiusCraft <meliuscraft@gmail.com>
2024-04-23 14:06:05 +0800

    feat(article): articleview show author other article

--- web/src/views/consumer/article/ArticleView.vue
+++ web/src/views/consumer/article/ArticleView.vue
@@ -1,63 +1,64 @@
 <template>
   <a-row gutter="20" v-if="articleData">
     <a-col :span="17">
+      <a-spin :loading="loadingState.article" tip="This may take a while..." style="width: 100%;">
+        <a-page-header :show-back="false" :style="{ background: 'var(--color-bg-2)', paddingBottom: '0px' }"
+          :title="articleData.title">
+          <template #breadcrumb>
+            <a-breadcrumb>
+              <router-link to="/"><a-breadcrumb-item>首页</a-breadcrumb-item></router-link>
+              <router-link
+                :to="`/article/${articleData.type.flag}`"><a-breadcrumb-item>文章列表</a-breadcrumb-item></router-link>
+              <a-breadcrumb-item>文章详情</a-breadcrumb-item>
+            </a-breadcrumb>
+          </template>
+          <template #subtitle>
+            <a-space>
 
-      <a-page-header :show-back="false" :style="{ background: 'var(--color-bg-2)', paddingBottom: '0px' }"
-        :title="articleData.title">
-        <template #breadcrumb>
-          <a-breadcrumb>
-            <router-link to="/"><a-breadcrumb-item>首页</a-breadcrumb-item></router-link>
-            <router-link
-              :to="`/article/${articleData.type.flag}`"><a-breadcrumb-item>文章列表</a-breadcrumb-item></router-link>
-            <a-breadcrumb-item>文章详情</a-breadcrumb-item>
-          </a-breadcrumb>
-        </template>
-        <template #subtitle>
-          <a-space>
+              <span><a-avatar :image-url="getFileUrl(articleData.user.avatar)"
+                  :style="{ marginRight: '8px', backgroundColor: '#165DFF' }" :size="28">
+                  load
+                </a-avatar>
+                <RouterLink :to="`/user/${articleData.user.id}`">{{ articleData.user.name }}</RouterLink>
+              </span>
+              <span>时间: {{ articleData.updatedAt }}</span>
+            </a-space>
+          </template>
+          <template #extra>
+            <a-space>
+              <AButtonGroup>
+                <AButton @click="like(articleData.id)">
+                  <icon-thumb-up-fill size="large" v-if="likeState" />
+                  <icon-thumb-up size="large" v-else />{{ articleData.like }}
+                </AButton>
 
-            <span><a-avatar :image-url="getFileUrl(articleData.user.avatar)"
-                :style="{ marginRight: '8px', backgroundColor: '#165DFF' }" :size="28">
-                load
-              </a-avatar>
-              <RouterLink :to="`/user/${articleData.user.id}`">{{ articleData.user.name }}</RouterLink>
-            </span>
-            <span>时间: {{ articleData.updatedAt }}</span>
-          </a-space>
-        </template>
-        <template #extra>
-          <a-space>
-            <AButtonGroup>
-              <AButton @click="like(articleData.id)">
-                <icon-thumb-up-fill size="large" v-if="likeState" />
-                <icon-thumb-up size="large" v-else />{{ articleData.like }}
-              </AButton>
+              </AButtonGroup>
+              <a-dropdown-button @click="subscribe(1, articleData.id)">
+                {{ articleSubscribe }}
+                <template #icon>
+                  <icon-down />
+                </template>
+                <template #content>
+                  <a-doption @click="subscribe(2, articleData.user.id)">{{ userSubscribe }}</a-doption>
+                </template>
+              </a-dropdown-button>
 
-            </AButtonGroup>
-            <a-dropdown-button @click="subscribe(1, articleData.id)">
-              {{ articleSubscribe }}
-              <template #icon>
-                <icon-down />
-              </template>
-              <template #content>
-                <a-doption @click="subscribe(2, articleData.user.id)">{{ userSubscribe }}</a-doption>
-              </template>
-            </a-dropdown-button>
-
-          </a-space>
-        </template>
-        <div>
-          <a-tag color=blue size="large">
-            {{ articleData.type.title || "未知" }}
-          </a-tag>
-          <a-divider direction="vertical" />
-          <a-space>
-            <a-tooltip v-for="tagItem in articleData.tags" :key="tagItem.name" :content="tagItem.description"><a-tag
-                color="red">{{ tagItem.name }}</a-tag></a-tooltip>
-          </a-space>
-        </div>
-      </a-page-header>
-      <MarkdownEdit :render-nav="setArticleNavs" preview-only :show-nav="false" v-model="articleData.content" />
-      <a-card :bordered="false" style="margin-top: 10px;">
+            </a-space>
+          </template>
+          <div>
+            <a-tag color=blue size="large">
+              {{ articleData.type.title || "未知" }}
+            </a-tag>
+            <a-divider direction="vertical" />
+            <a-space>
+              <a-tooltip v-for="tagItem in articleData.tags" :key="tagItem.name" :content="tagItem.description"><a-tag
+                  color="red">{{ tagItem.name }}</a-tag></a-tooltip>
+            </a-space>
+          </div>
+        </a-page-header>
+        <MarkdownEdit :render-nav="setArticleNavs" preview-only :show-nav="false" v-model="articleData.content" />
+      </a-spin>
+      <a-card :loading="loadingState.comment" :bordered="false" style="margin-top: 10px;">
         <comment-edit :callback="getRootComment" :article-id="articleData.id" style="margin-bottom: 15px;" />
         <template v-if="articleCommentList.length">
           <comment-item :article="articleData" :callback="getRootComment" v-for="comment in articleCommentList"
@@ -71,14 +72,37 @@
       </a-card>
     </a-col>
     <a-col span="7">
-      <UserInfoCard :user-id="articleData.user.id" />
-      <a-affix>
-      <a-card style="margin-top: 10px;max-height: 400px;" :bordered="false" title="目录">
-        <a-anchor smooth style="width: 100%;max-height: 300px;" >
-          <MyAnchorLink v-for="item in articleNavs" :data="item" style="width: 100%;"/>
-      </a-anchor>
-      </a-card>
-    </a-affix>
+      <a-grid row-gap="5" cols="1">
+        <a-col>
+          <UserInfoCard :user-id="articleData.user.id" />
+        </a-col>
+        <a-col>
+          <a-affix v-if="articleNavs.length > 0">
+            <a-card style="margin-top: 10px;max-height: 400px;" :bordered="false" title="目录">
+              <a-anchor smooth style="width: 100%;max-height: 300px;">
+                <MyAnchorLink v-for="item in articleNavs" :data="item" style="width: 100%;" />
+              </a-anchor>
+            </a-card>
+          </a-affix>
+        </a-col>
+        <a-col>
+          <a-card :bordered="false" body-style="padding: 0px;" title="用户文章">
+            <a-list :loading="loadingState.userlink" :split="false" :size="'small'" :bordered="false"
+              :data="userLinkArticles">
+              <template #item="{ item }">
+                <a-list-item class="list-demo-item" style="padding: 0 9px;" action-layout="vertical"
+                  @click="router.push(`/article/view/${item.id}`)">
+                  <a-list-item-meta :title="item.title">
+                    <template #description>
+                      <small>{{ item.like }}点赞</small>
+                    </template>
+                  </a-list-item-meta>
+                </a-list-item>
+              </template>
+            </a-list>
+          </a-card>
+        </a-col>
+      </a-grid>
     </a-col>
   </a-row>
 </template>
@@ -87,24 +111,31 @@
 import MyAnchorLink from '@/components/MyAnchorLink.vue'
 import UserInfoCard from '@/components/user/UserInfoCard.vue'
 import { apiSubscribe, apiSubscribeState, } from "@/apis/apiSubscribe.js";
-import { apiArticleLike, apiArticleLikeState, apiArticleView } from '@/apis/article';
+import { apiArticleLike, apiArticleLikeState, apiArticleList, apiArticleView } from '@/apis/article';
 import { apiGetArticleComment } from '@/apis/comment';
 import { apiGetFile } from "@/apis/file";
 import CommentEdit from '@/components/comment/CommentEdit.vue';
 import CommentItem from '@/components/comment/CommentItem.vue';
 import router from "@/router";
-import { IconDown, IconThumbUp, IconThumbUpFill } from '@arco-design/web-vue/es/icon';
+import { IconDown, IconThumbUp, IconThumbUpFill, IconHeart } from '@arco-design/web-vue/es/icon';
 import 'cherry-markdown/dist/cherry-markdown.css';
 import { watch } from "vue";
 import { useRoute } from 'vue-router';
 import MarkdownEdit from '@/components/MarkdownEdit.vue';
 import { ref } from 'vue';
+import { reactive } from 'vue';
 const getFileUrl = (fileKey) => apiGetFile(fileKey)
 
 const articleData = ref(null)
 const articleNavs = ref([])
 const route = useRoute()
 const likeState = ref(true)
+const userLinkArticles = ref([])
+const loadingState = reactive({
+  comment: true,
+  userlink: true,
+  article: true
+})
 
 const articleSubscribe = ref("订阅文章")
 
@@ -123,9 +154,12 @@ function getRootComment() {
     }
     paginationData.value.total = data.count
     articleCommentList.value = data.data
-  })
+  }).finally(() => loadingState.comment = false)
 }
 watch(() => route.fullPath, () => {
+  loadingState.userlink = true
+  loadingState.comment = true
+  loadingState.article = true
   apiArticleView(route.params.id).then(({ data }) => {
     articleData.value = data
     const id = articleData.value.id
@@ -138,10 +172,14 @@ watch(() => route.fullPath, () => {
     apiSubscribeState(2, articleData.value.user.id).then((data) => {
       userSubscribe.value = data.data ? "用户已订阅" : "订阅用户"
     })
+    apiArticleList({ userId: data.user.id, state: 2 }, 1, 5).then(({ data, ok }) => {
+      if (!ok) return
+      userLinkArticles.value = data.list
+    }).finally(() => loadingState.userlink = false)
     getRootComment()
   }).catch(() => {
     router.back()
-  })
+  }).finally(() => loadingState.article = false)
 }, { immediate: true })
 
 function like(id) {
@@ -166,7 +204,7 @@ function arrayToTree(arr, level = 1) {
 
   for (let i = 0; i < arr.length; i++) {
     const item = arr[i];
-    if(level != 1 && item.level == 1) break
+    if (level != 1 && item.level == 1) break
     if (item.level === level) {
       const node = {
         id: item.id,
@@ -182,11 +220,11 @@ function arrayToTree(arr, level = 1) {
 
 function setArticleNavs(arr) {
   console.log(arr)
-  articleNavs.value =  arrayToTree(arr)
+  articleNavs.value = arrayToTree(arr)
 }
 </script>
 
-<style>
+<style scoped>
 .arco-dropdown-open .arco-icon-down {
   transform: rotate(180deg);
 }
@@ -197,5 +235,11 @@ function setArticleNavs(arr) {
 
 .cherry-flex-toc {
   position: fixed
+}
+
+.list-demo-item:hover {
+  transition: background-color 0.3s ease;
+  background-color: #f4f4f491;
+  cursor: pointer;
 }
 </style>
